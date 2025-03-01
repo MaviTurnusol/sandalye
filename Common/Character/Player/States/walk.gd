a46d59f03c4bbfc5215143f83b771e2d4260beba
MaviTurnusol <betatorium@gmail.com>
2025-03-01 17:40:20 +0300

    walk state animasyon update

--- Common/Character/Player/States/walk.gd
+++ Common/Character/Player/States/walk.gd
@@ -1,5 +1,19 @@
 extends State
+
 @export var speed := 150.0
+var input := Vector2.ZERO : set = set_direction
+
+func set_direction(value):
+	if input.x > 0 && value.x <= 0:
+		grandfather.playAnimationFullBody("turn")
+	if input.x < 0 && value.x >= 0:
+		grandfather.playAnimationFullBody("turn")
+	if value.x == 0 && input.x != 0:
+		grandfather.playAnimationConsequently("turnBackwards", "idle")
+	if value.x != 0 && input.x == 0:
+		grandfather.playAnimationConsequently("turn", "walk")
+	input = value
+	pass
 func _ready():
 	super._ready()
 	conditions.append(grandfather.is_on_floor)
@@ -12,9 +26,11 @@ func Enter():
 	endAnim = "turnBackwards"
 	pass
 func Physics_Update(_delta):
-	var input = Vector2(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), 0)
+	input = Vector2(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), 0)
 	if input.x != 0:
 		grandfather.velocity.x = lerp(grandfather.velocity.x, input.x * speed, 0.1)
+	else:
+		grandfather.velocity.x = lerp(grandfather.velocity.x, 0.0, 0.2)
 	if input.x < 0:
 		grandfather.TurnSprites("left")
 	elif input.x > 0:
